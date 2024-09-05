9809a043cc3c6c76240653de467f5c21fcf09f06
MdWajidAli <imdwajidali@gmail.com>
2024-09-05 15:21:08 +0530

    completed navbar functionality

---
+++ app/properties/add/page.jsx
@@ -0,0 +1,7 @@
+const AddPropertyPage = () => {
+    return <div>
+        Add Property
+    </div>
+}
+
+export default AddPropertyPage;
---
+++ app/properties/page.jsx
@@ -0,0 +1,9 @@
+const PropertiesPage = () => {
+    return (
+        <div className="text-3xl">
+            Properties Page
+        </div>
+    )
+}
+
+export default PropertiesPage;
